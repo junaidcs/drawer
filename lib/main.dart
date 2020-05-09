b214d1d6c205a3d0cf9fe5e247c35da44b00a322
Muhammad Junaid <m.junaidcs@gmail.com>
2020-05-09 21:29:46 +0500

    custom drawer tile

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,45 @@ class DrawerApp extends StatelessWidget {
           title: Text('Drawer Application'),
           backgroundColor: Colors.deepOrange,
         ),
-        
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[Colors.deepOrange, Colors.orangeAccent],
+                  ),
+                ),
+                child: Text('Some header'),
+              ),
+              CustomDrawerTile(),
+              CustomDrawerTile(),
+              CustomDrawerTile(),
+              CustomDrawerTile(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomDrawerTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Icon(Icons.person),
+              Text('Profile'),
+            ],
+          ),
+          Icon(Icons.arrow_right),
+        ],
       ),
     );
   }
