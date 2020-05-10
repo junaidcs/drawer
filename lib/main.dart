88fa3512a246cab2a650844939032e1084d5d89e
Muhammad Junaid <m.junaidcs@gmail.com>
2020-05-10 16:19:03 +0500

    added drawer header with image and text

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:drawer/widgets/custom_drawer_tile.dart';
 
-void main() => runApp(DrawerApp());
+void main() => runApp(MyApp());
 
-class DrawerApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,11 +22,38 @@ class DrawerApp extends StatelessWidget {
                     colors: <Color>[Colors.deepOrange, Colors.orangeAccent],
                   ),
                 ),
-                child: Text('Some header'),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Material(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Image.asset(
+                          'assets/images/lego.jpg',
+                          width: 80,
+                          height: 80,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Text(
+                        'Logo Carlos',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
               ),
               CustomDrawerTile(Icons.person, 'Profile', Icons.arrow_right),
               CustomDrawerTile(Icons.settings, 'Settings', Icons.arrow_right),
-              CustomDrawerTile(Icons.notifications, 'Notifications', Icons.arrow_right),
+              CustomDrawerTile(
+                  Icons.notifications, 'Notifications', Icons.arrow_right),
               CustomDrawerTile(Icons.lock, 'Logout', Icons.arrow_right),
             ],
           ),
